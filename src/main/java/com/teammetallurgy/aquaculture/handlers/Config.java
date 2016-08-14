d293552fe790e3dd0e8d730b0fa3f3b87688e604
Glassmaker <JeanGlassmaker@mail.com>
2016-08-14 10:36:22 +0300

    Removes Enchantment IDs from config

--- src/main/java/com/teammetallurgy/aquaculture/handlers/Config.java
+++ src/main/java/com/teammetallurgy/aquaculture/handlers/Config.java
@@ -9,20 +9,6 @@ import java.util.Map.Entry;
 
 public class Config {
 
-    /*
-     * Enchantments
-     */
-    public static final String categoryEnchantments = "ENCHANTMENTS";
-
-    public static int appealingID = 60;
-    public static int magneticID = 69;
-    public static int longcastID = 70;
-    public static int shortcastID = 63;
-    public static int fastcastID = 64;
-    public static int barbedHookID = 65;
-    public static int doubleHookID = 66;
-    public static int heavyLineID = 68;
-
     public static final String categoryFishRarity = "FISH_RARITY";
     public static HashMap<String, Integer> fishRarity = new HashMap<String, Integer>();
 
@@ -102,15 +88,6 @@ public class Config {
     public void init(File file) {
         Configuration config = new Configuration(file);
         config.load();
-
-        appealingID = config.get(categoryEnchantments, "Appealing", appealingID).getInt(appealingID);
-        magneticID = config.get(categoryEnchantments, "Magnetic", magneticID).getInt(magneticID);
-        longcastID = config.get(categoryEnchantments, "Long Cast", longcastID).getInt(longcastID);
-        shortcastID = config.get(categoryEnchantments, "Short Cast", shortcastID).getInt(shortcastID);
-        // fastcastID = isIdAvailableOrSet(config.get(categoryEnchantments, "Fast Cast", fastcastID)).getInt();
-        barbedHookID = config.get(categoryEnchantments, "Barbed Hook", barbedHookID).getInt(barbedHookID);
-        doubleHookID = config.get(categoryEnchantments, "Double Hook", doubleHookID).getInt(doubleHookID);
-        heavyLineID = config.get(categoryEnchantments, "Heavy Line", heavyLineID).getInt(heavyLineID);
 
         for (Entry<String, Integer> entry : fishRarity.entrySet()) {
             int rarity = entry.getValue();
