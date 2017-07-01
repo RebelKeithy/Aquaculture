503d6c0e1f3ee026bc7743ef47837ba57046cf28
Glassmaker <JeanGlassmaker@mail.com>
2017-07-01 14:31:11 +0300

    Adds shapeless JSON recipes

--- src/main/java/com/teammetallurgy/aquaculture/handlers/AquacultureRecipes.java
+++ src/main/java/com/teammetallurgy/aquaculture/handlers/AquacultureRecipes.java
@@ -9,26 +9,11 @@ public class AquacultureRecipes {
 
         // Fixme: Change to JSON recipes
         /*
-        GameRegistry.addShapelessRecipe(AquacultureItems.whaleSteak.getItemStack(5), AquacultureItems.fish.getItemStackFish("Whale"));
-        GameRegistry.addShapelessRecipe(new ItemStack(Items.DYE, 2, 0), AquacultureItems.fish.getItemStackFish("Squid"));
-        GameRegistry.addShapelessRecipe(new ItemStack(Blocks.RED_MUSHROOM, 5), AquacultureItems.fish.getItemStackFish("Red Shrooma"));
-        GameRegistry.addShapelessRecipe(new ItemStack(Blocks.BROWN_MUSHROOM, 5), AquacultureItems.fish.getItemStackFish("Brown Shrooma"));
-        GameRegistry.addShapelessRecipe(new ItemStack(Items.LEATHER, 2), new ItemStack(Items.LEATHER_BOOTS));
-        GameRegistry.addShapelessRecipe(new ItemStack(Items.GOLD_NUGGET, 1), AquacultureItems.fish.getItemStackFish("Goldfish"));
-        GameRegistry.addShapelessRecipe(AquacultureItems.frogLegs.getItemStack(1), AquacultureItems.fish.getItemStackFish("Frog"));
-        GameRegistry.addShapelessRecipe(AquacultureItems.turtleSoup.getItemStack(), AquacultureItems.fish.getItemStackFish("Turtle"), Items.WATER_BUCKET);
-        GameRegistry.addShapelessRecipe(AquacultureItems.sushi.getItemStack(), AquacultureItems.fishFillet.getItemStack(), AquacultureItems.seaweed.getItemStack());
-        GameRegistry.addShapelessRecipe(new ItemStack(Items.DYE, 3, 15), AquacultureItems.fish.getItemStackFish("Fish Bones"));
-        
-        GameRegistry.addShapelessRecipe(new ItemStack(Blocks.PLANKS, 4), AquacultureItems.driftwood.getItemStack());
-        
         GameRegistry.addSmelting(AquacultureItems.fishFillet.getItemStack(), AquacultureItems.cookedFillet.getItemStack(), 0.3f);
         GameRegistry.addSmelting(AquacultureItems.whaleSteak.getItemStack(), AquacultureItems.cookedWhaleSteak.getItemStack(), 0.3f);
         GameRegistry.addSmelting(AquacultureItems.tinCan.getItemStack(), new ItemStack(Items.IRON_INGOT), 0.7f);
         GameRegistry.addSmelting(AquacultureItems.frogLegs.getItemStack(), AquacultureItems.cookedFrogLegs.getItemStack(), 0.3F);
         
-        GameRegistry.addShapelessRecipe(new ItemStack(AquacultureItems.woodenFishingRod), new ItemStack(Items.FISHING_ROD));
-        GameRegistry.addShapelessRecipe(new ItemStack(Items.FISHING_ROD), new ItemStack(AquacultureItems.woodenFishingRod));
         GameRegistry.addShapedRecipe(new ItemStack(AquacultureItems.ironFishingRod, 1), "  X", " XS", "I S", 'X', Items.IRON_INGOT, 'I', Items.STICK, 'S', Items.STRING);
         GameRegistry.addShapedRecipe(new ItemStack(AquacultureItems.ironFishingRod, 1), "  X", " XS", "I S", 'X', Items.IRON_INGOT, 'I', Items.STICK, 'S', Items.STRING);
         GameRegistry.addShapedRecipe(new ItemStack(AquacultureItems.goldFishingRod, 1), "  X", " XS", "I S", 'X', Items.GOLD_INGOT, 'I', Items.STICK, 'S', Items.STRING);
