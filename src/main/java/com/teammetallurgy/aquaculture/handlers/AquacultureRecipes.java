69a481cba617b14a8d725103878f0702b3a469d3
GirafiStudios <girafi0@gmail.com>
2018-05-04 04:44:29 +0200

    Added Jellyfish to slime ball smelting recipe

--- src/main/java/com/teammetallurgy/aquaculture/handlers/AquacultureRecipes.java
+++ src/main/java/com/teammetallurgy/aquaculture/handlers/AquacultureRecipes.java
@@ -15,6 +15,7 @@ public class AquacultureRecipes {
         GameRegistry.addSmelting(AquacultureItems.fishFillet.getItemStack(), AquacultureItems.cookedFillet.getItemStack(), 0.3F);
         GameRegistry.addSmelting(AquacultureItems.whaleSteak.getItemStack(), AquacultureItems.cookedWhaleSteak.getItemStack(), 0.3F);
         GameRegistry.addSmelting(AquacultureItems.frogLegs.getItemStack(), AquacultureItems.cookedFrogLegs.getItemStack(), 0.3F);
+        GameRegistry.addSmelting(AquacultureItems.fish.getItemStackFish("Jellyfish"), new ItemStack(Items.SLIME_BALL), 0.3F);
         GameRegistry.addSmelting(AquacultureItems.tinCan.getItemStack(), OreDictionary.doesOreNameExist("nuggetTin") ? new ItemStack(getOreDict("nuggetTin").getItem(), 7, getOreDict("nuggetTin").getItemDamage()) : new ItemStack(Items.IRON_NUGGET, 7), 0.7F);
     }
 
