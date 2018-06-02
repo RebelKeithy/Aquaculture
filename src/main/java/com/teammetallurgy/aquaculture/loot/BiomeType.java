0c6a3b524900e5a4f9fb48bd63e6337daa5f47f1
GirafiStudios <girafi0@gmail.com>
2018-06-02 05:38:46 +0200

    Fixed biomes with multiple BiomeTypes not working properly, Closes #40

--- src/main/java/com/teammetallurgy/aquaculture/loot/BiomeType.java
+++ src/main/java/com/teammetallurgy/aquaculture/loot/BiomeType.java
@@ -6,13 +6,13 @@ import java.util.Map;
 public class BiomeType {
     public static BiomeType freshwater = new BiomeType("Freshwater", new int[] { 1, 4, 6, 7, 14, 15, 18, 27, 28, 29, 33, 129, 132, 134, 155, 156, 157, 161 });
     public static BiomeType arid = new BiomeType("Arid", new int[] { 2, 17, 35, 36, 37, 38, 39, 130, 133, 163, 164, 165, 166, 167 });
-    public static BiomeType arctic = new BiomeType("Arctic", new int[] { 3, 5, 10, 11, 12, 13, 19, 30, 25, 26, 30, 31, 32, 34, 131, 140, 158, 160, 162 });
+    public static BiomeType arctic = new BiomeType("Arctic", new int[] { 3, 5, 10, 11, 12, 13, 19, 25, 26, 30, 31, 32, 34, 131, 140, 158, 160, 162 });
     public static BiomeType saltwater = new BiomeType("Saltwater", new int[] { 0, 16, 10, 24, 26 });
     public static BiomeType tropical = new BiomeType("Tropical", new int[] { 21, 22, 23, 149, 151 });
     public static BiomeType brackish = new BiomeType("Brackish", new int[] { 6, 134 });
     public static BiomeType mushroom = new BiomeType("Mushroom", new int[] { 14, 15 });
 
-    protected static Map<Integer, BiomeType> biomeMap;
+    private static Map<Integer, BiomeType> biomeMap;
 
     private String name;
 
@@ -25,11 +25,12 @@ public class BiomeType {
     }
 
     public static void addBiome(int biomeID, BiomeType biomeType) {
-        if (biomeMap == null)
+        if (biomeMap == null) {
             biomeMap = new HashMap<>();
+        }
 
-        if (biomeMap.containsKey(biomeID)) {
-            System.out.println("[Aquaculture] Error: Biome ID " + biomeID + " already registered as " + biomeMap.get(biomeID) + " when trying to register " + biomeType);
+        if (biomeMap.containsKey(biomeID) && biomeMap.get(biomeID) == biomeType) {
+            System.out.println("[Aquaculture] Error: Biome ID " + biomeID + " is already registered as " + biomeMap.get(biomeID));
         } else {
             biomeMap.put(biomeID, biomeType);
         }
