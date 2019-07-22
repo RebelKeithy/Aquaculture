f848a2b6ee600df5b0db63b84e288ed0971fb195
GirafiStudios <faj10@msn.com>
2019-07-22 12:42:44 +0200

    Initial work on Neptunium armor effects

--- src/main/java/com/teammetallurgy/aquaculture/item/NeptuniumArmor.java
+++ src/main/java/com/teammetallurgy/aquaculture/item/NeptuniumArmor.java
@@ -1,14 +1,24 @@
 package com.teammetallurgy.aquaculture.item;
 
+import com.teammetallurgy.aquaculture.Aquaculture;
+import com.teammetallurgy.aquaculture.init.AquaItems;
 import net.minecraft.entity.Entity;
+import net.minecraft.entity.MoverType;
+import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.inventory.EquipmentSlotType;
 import net.minecraft.item.ArmorItem;
 import net.minecraft.item.IArmorMaterial;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
+import net.minecraft.potion.EffectInstance;
+import net.minecraft.potion.Effects;
+import net.minecraft.tags.FluidTags;
+import net.minecraft.world.World;
+import net.minecraftforge.fml.common.Mod;
 
 import javax.annotation.Nonnull;
 
+@Mod.EventBusSubscriber(modid = Aquaculture.MOD_ID)
 public class NeptuniumArmor extends ArmorItem {
     private String texture;
 
@@ -16,13 +26,30 @@ public class NeptuniumArmor extends ArmorItem {
         super(armorMaterial, equipmentSlot, builder);
     }
 
+    @Override
+    public void onArmorTick(@Nonnull ItemStack stack, World world, PlayerEntity player) {
+        if (player.areEyesInFluid(FluidTags.WATER)) {
+            if (this.slot == EquipmentSlotType.HEAD) {
+                player.addPotionEffect(new EffectInstance(Effects.NIGHT_VISION, 1, 0, false, false, false));
+            } else if (this.slot == EquipmentSlotType.CHEST) {
+                player.addPotionEffect(new EffectInstance(Effects.WATER_BREATHING, 1, 0, false, false, false));
+            } else if(this.slot == EquipmentSlotType.LEGS) {
+                System.out.println(player.getMotion().y);
+                player.setMotion(player.getMotion().add(0, player.getMotion().y, 0));
+            } else if (this.slot == EquipmentSlotType.FEET) {
+                player.addPotionEffect(new EffectInstance(Effects.SPEED, 1, 0, false, false, false));
+                player.move(MoverType.PLAYER, player.getMotion()); //Make the swimming react to the swiftness potion
+            }
+        }
+    }
+
     public Item setArmorTexture(String string) {
-        texture = string;
+        this.texture = string;
         return this;
     }
 
     @Override
     public String getArmorTexture(@Nonnull ItemStack stack, Entity entity, EquipmentSlotType slot, String layer) {
-        return "aquaculture:textures/armor/" + texture + ".png";
+        return "aquaculture:textures/armor/" + this.texture + ".png";
     }
 }
--- src/main/java/com/teammetallurgy/aquaculture/misc/AquaTooltip.java
+++ src/main/java/com/teammetallurgy/aquaculture/misc/AquaTooltip.java
@@ -7,12 +7,13 @@ import net.minecraft.client.util.InputMappings;
 import net.minecraft.item.Item;
 import net.minecraft.util.text.TextFormatting;
 import net.minecraft.util.text.TranslationTextComponent;
+import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.event.entity.player.ItemTooltipEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
 import org.lwjgl.glfw.GLFW;
 
-@Mod.EventBusSubscriber(modid = Aquaculture.MOD_ID)
+@Mod.EventBusSubscriber(modid = Aquaculture.MOD_ID, value = Dist.CLIENT)
 public class AquaTooltip {
 
     @SubscribeEvent
@@ -24,7 +25,6 @@ public class AquaTooltip {
                     String itemIdentifier = item.getRegistryName().getPath() + ".tooltip";
                     if (InputMappings.isKeyDown(Minecraft.getInstance().mainWindow.getHandle(), GLFW.GLFW_KEY_LEFT_SHIFT)) {
                         event.getToolTip().add(new TranslationTextComponent(Aquaculture.MOD_ID + "." + itemIdentifier + ".line1").applyTextStyle(TextFormatting.AQUA));
-                        //event.getToolTip().add(new TranslationTextComponent(Aquaculture.MOD_ID + "." + itemIdentifier + ".line2").applyTextStyle(TextFormatting.AQUA));
                     } else {
                         event.getToolTip().add(new TranslationTextComponent(Aquaculture.MOD_ID + "." + itemIdentifier + ".title").applyTextStyle(TextFormatting.AQUA)
                                 .appendText(" ").appendSibling(new TranslationTextComponent(Aquaculture.MOD_ID + ".shift").applyTextStyle(TextFormatting.DARK_GRAY)));
