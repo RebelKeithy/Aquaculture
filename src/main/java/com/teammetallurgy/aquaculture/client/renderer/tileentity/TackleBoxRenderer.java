d66827aaa479aa84fcb7d5bfdce3751226b61577
GirafiStudios <faj10@msn.com>
2019-08-17 18:43:12 +0200

    Initial work on Tackle Box renderer

---
+++ src/main/java/com/teammetallurgy/aquaculture/client/renderer/tileentity/TackleBoxRenderer.java
@@ -0,0 +1,8 @@
+package com.teammetallurgy.aquaculture.client.renderer.tileentity;
+
+import com.teammetallurgy.aquaculture.block.tileentity.TackleBoxTileEntity;
+import net.minecraft.client.renderer.tileentity.TileEntityRenderer;
+
+public class TackleBoxRenderer extends TileEntityRenderer<TackleBoxTileEntity> {
+
+}
