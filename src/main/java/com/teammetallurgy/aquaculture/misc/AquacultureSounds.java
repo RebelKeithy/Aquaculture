0989884f0ba80196349f6c4834bfbf994f1e257b
sscards55 <free-4-mel@hotmail.com>
2020-11-28 22:08:28 +0900

    Fix name of sound event builder method (#172)

--- src/main/java/com/teammetallurgy/aquaculture/misc/AquacultureSounds.java
+++ src/main/java/com/teammetallurgy/aquaculture/misc/AquacultureSounds.java
@@ -11,25 +11,25 @@ import net.minecraftforge.registries.IForgeRegistry;
 
 @Mod.EventBusSubscriber(modid = Aquaculture.MOD_ID, bus = Mod.EventBusSubscriber.Bus.MOD)
 public final class AquacultureSounds{
-    public static final SoundEvent TACKLE_BOX_OPEN = registersound("tackle_box_open");
-    public static final SoundEvent TACKLE_BOX_CLOSE = registersound("tackle_box_close");
-    public static final SoundEvent WORM_FARM_EMPTY = registersound("worm_farm_empty");
-    public static final SoundEvent FISH_MOUNT_REMOVED = registersound("fish_mount_removed");
-    public static final SoundEvent FISH_MOUNT_BROKEN = registersound("fish_mount_broken");
-    public static final SoundEvent FISH_MOUNT_PLACED = registersound("fish_mount_placed");
-    public static final SoundEvent FISH_MOUNT_ADD_ITEM = registersound("fish_mount_add_item");
-    public static final SoundEvent BOBBER_BAIT_BREAK = registersound("bobber_bait");
-    public static final SoundEvent BOBBER_LAND_IN_LAVA = registersound("bobber_land_lava");
-    public static final SoundEvent JELLYFISH_FLOP = registersound("jellyfish_flop");
-    public static final SoundEvent FISH_FLOP = registersound("fish_flop");
-    public static final SoundEvent FISH_AMBIENT = registersound("fish_ambient");
-    public static final SoundEvent FISH_DEATH = registersound("fish_death");
-    public static final SoundEvent FISH_HURT = registersound("fish_hurt");
-    public static final SoundEvent FISH_COLLIDE = registersound("fish_collide");
-    public static final SoundEvent BOTTLE_OPEN = registersound("bottle_open");
-    public static final SoundEvent BOBBER_NOTE = registersound("bobber_note_catch");
+    public static final SoundEvent TACKLE_BOX_OPEN = registerSound("tackle_box_open");
+    public static final SoundEvent TACKLE_BOX_CLOSE = registerSound("tackle_box_close");
+    public static final SoundEvent WORM_FARM_EMPTY = registerSound("worm_farm_empty");
+    public static final SoundEvent FISH_MOUNT_REMOVED = registerSound("fish_mount_removed");
+    public static final SoundEvent FISH_MOUNT_BROKEN = registerSound("fish_mount_broken");
+    public static final SoundEvent FISH_MOUNT_PLACED = registerSound("fish_mount_placed");
+    public static final SoundEvent FISH_MOUNT_ADD_ITEM = registerSound("fish_mount_add_item");
+    public static final SoundEvent BOBBER_BAIT_BREAK = registerSound("bobber_bait");
+    public static final SoundEvent BOBBER_LAND_IN_LAVA = registerSound("bobber_land_lava");
+    public static final SoundEvent JELLYFISH_FLOP = registerSound("jellyfish_flop");
+    public static final SoundEvent FISH_FLOP = registerSound("fish_flop");
+    public static final SoundEvent FISH_AMBIENT = registerSound("fish_ambient");
+    public static final SoundEvent FISH_DEATH = registerSound("fish_death");
+    public static final SoundEvent FISH_HURT = registerSound("fish_hurt");
+    public static final SoundEvent FISH_COLLIDE = registerSound("fish_collide");
+    public static final SoundEvent BOTTLE_OPEN = registerSound("bottle_open");
+    public static final SoundEvent BOBBER_NOTE = registerSound("bobber_note_catch");
 
-    private static SoundEvent makeSoundEvent(String name) {
+    private static SoundEvent registerSound(String name) {
         ResourceLocation location = new ResourceLocation("aquaculture", name);
         return (SoundEvent)(new SoundEvent(location)).setRegistryName(location);
     }
