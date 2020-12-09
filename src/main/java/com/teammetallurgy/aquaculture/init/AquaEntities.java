866266eca064a4d6bc0de359c9fd1dab65811cb2
GirafiStudios <faj10@msn.com>
2020-12-09 14:10:20 +0100

    Changed entity spawning back to using BiomeDictionary

--- src/main/java/com/teammetallurgy/aquaculture/init/AquaEntities.java
+++ src/main/java/com/teammetallurgy/aquaculture/init/AquaEntities.java
@@ -5,9 +5,8 @@ import com.teammetallurgy.aquaculture.Aquaculture;
 import com.teammetallurgy.aquaculture.entity.AquaFishingBobberEntity;
 import com.teammetallurgy.aquaculture.entity.TurtleLandEntity;
 import com.teammetallurgy.aquaculture.entity.WaterArrowEntity;
-import com.teammetallurgy.aquaculture.loot.BiomePropertiesPredicate;
 import com.teammetallurgy.aquaculture.misc.AquaConfig;
-import com.teammetallurgy.aquaculture.misc.BiomeHelper;
+import com.teammetallurgy.aquaculture.misc.BiomeDictionaryHelper;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.EntityClassification;
 import net.minecraft.entity.EntitySpawnPlacementRegistry;
@@ -17,8 +16,8 @@ import net.minecraft.item.Item;
 import net.minecraft.item.ItemGroup;
 import net.minecraft.item.SpawnEggItem;
 import net.minecraft.util.ResourceLocation;
-import net.minecraft.world.biome.Biome;
 import net.minecraft.world.gen.Heightmap;
+import net.minecraftforge.common.BiomeDictionary;
 import net.minecraftforge.event.RegistryEvent;
 import net.minecraftforge.event.world.BiomeLoadingEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
@@ -47,13 +46,13 @@ public class AquaEntities {
     public static final EntityType<WaterArrowEntity> WATER_ARROW = register("water_arrow", EntityType.Builder.<WaterArrowEntity>create(WaterArrowEntity::new, EntityClassification.MISC)
             .size(0.5F, 0.5F)
             .setCustomClientFactory(WaterArrowEntity::new));
-    public static final EntityType<TurtleLandEntity> BOX_TURTLE = registerMob("box_turtle", 1, 2, 9, Biome.Category.SWAMP, null, null, null, 0x7F8439, 0x5D612A,
+    public static final EntityType<TurtleLandEntity> BOX_TURTLE = registerMob("box_turtle", 1, 2, 9, BiomeDictionary.Type.SWAMP, null, 0x7F8439, 0x5D612A,
             EntityType.Builder.create(TurtleLandEntity::new, EntityClassification.CREATURE)
                     .size(0.5F, 0.25F));
-    public static final EntityType<TurtleLandEntity> ARRAU_TURTLE = registerMob("arrau_turtle", 1, 2, 6, Biome.Category.JUNGLE, null, null, null, 0x71857A, 0x4F6258,
+    public static final EntityType<TurtleLandEntity> ARRAU_TURTLE = registerMob("arrau_turtle", 1, 2, 6, BiomeDictionary.Type.JUNGLE, null, 0x71857A, 0x4F6258,
             EntityType.Builder.create(TurtleLandEntity::new, EntityClassification.CREATURE)
                     .size(0.5F, 0.25F));
-    public static final EntityType<TurtleLandEntity> STARSHELL_TURTLE = registerMob("starshell_turtle", 1, 2, 5, null, null, null, null, 0xDCE2E5, 0x464645,
+    public static final EntityType<TurtleLandEntity> STARSHELL_TURTLE = registerMob("starshell_turtle", 1, 2, 5, BiomeDictionaryHelper.TWILIGHT, null, 0xDCE2E5, 0x464645,
             EntityType.Builder.create(TurtleLandEntity::new, EntityClassification.CREATURE)
                     .size(0.5F, 0.25F));
 
@@ -64,15 +63,15 @@ public class AquaEntities {
         }
     }
 
-    private static <T extends Entity> EntityType<T> registerMob(String name, int min, int max, int weight, @Nullable Biome.Category include, @Nullable Biome.Category exclude, BiomePropertiesPredicate.TemperatureType temperature, Biome.RainType rainType, int eggPrimary, int eggSecondary, EntityType.Builder<T> builder) {
-        return registerMob(name, min, max, weight, eggPrimary, eggSecondary, Collections.singletonList(String.valueOf(include == null ? "" : include)), Collections.singletonList(String.valueOf(exclude == null ? "" : exclude)), Collections.singletonList(String.valueOf(temperature == null ? "" : temperature)), Collections.singletonList(String.valueOf(rainType == null ? "" : rainType)), builder);
+    private static <T extends Entity> EntityType<T> registerMob(String name, int min, int max, int weight, BiomeDictionary.Type include, @Nullable BiomeDictionary.Type exclude, int eggPrimary, int eggSecondary, EntityType.Builder<T> builder) {
+        return registerMob(name, min, max, weight, eggPrimary, eggSecondary, Collections.singletonList(String.valueOf(include == null ? "" : include)), Collections.singletonList(String.valueOf(exclude == null ? "" : exclude)), builder);
     }
 
-    private static <T extends Entity> EntityType<T> registerMob(String name, int min, int max, int weight, int eggPrimary, int eggSecondary, List<? extends String> include, List<? extends String> exclude, List<? extends String> temperatures, List<? extends String> rainTypes, EntityType.Builder<T> builder) {
+    private static <T extends Entity> EntityType<T> registerMob(String name, int min, int max, int weight, int eggPrimary, int eggSecondary, List<? extends String> include, List<? extends String> exclude, EntityType.Builder<T> builder) {
         EntityType<T> entityType = register(name, builder);
         Item spawnEgg = new SpawnEggItem(entityType, eggPrimary, eggSecondary, (new Item.Properties()).group(ItemGroup.MISC));
         AquaItems.register(spawnEgg, name + "_spawn_egg");
-        new AquaConfig.Spawn(AquaConfig.BUILDER, name, min, max, weight, include, exclude, temperatures, rainTypes);
+        new AquaConfig.Spawn(AquaConfig.BUILDER, name, min, max, weight, include, exclude);
         MOBS.add(entityType);
         return entityType;
     }
@@ -98,7 +97,7 @@ public class AquaEntities {
         for (EntityType<?> entityType : MOBS) {
             String name = Objects.requireNonNull(entityType.getRegistryName()).getPath();
             String subCategory = Helper.getSubConfig(AquaConfig.Spawn.SPAWN_OPTIONS, name);
-            BiomeHelper.addSpawn(entityType, Helper.get(subCategory, "min"), Helper.get(subCategory, "max"), Helper.get(subCategory, "weight"), Helper.get(subCategory, "include"), Helper.get(subCategory, "exclude"), Helper.get(subCategory, "temperatures"), Helper.get(subCategory, "rain_types"), event);
+            BiomeDictionaryHelper.addSpawn(entityType, Helper.get(subCategory, "min"), Helper.get(subCategory, "max"), Helper.get(subCategory, "weight"), Helper.get(subCategory, "include"), Helper.get(subCategory, "exclude"), event);
         }
     }
 }
