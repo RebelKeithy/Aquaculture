bbf07492f515c8cf7dcbe7840fbf7b061d5f1cb2
GirafiStudios <faj10@msn.com>
2021-08-06 22:13:46 +0200

    Remove unused localization strings

--- src/main/java/com/teammetallurgy/aquaculture/item/neptunium/NeptuniumHoe.java
+++ src/main/java/com/teammetallurgy/aquaculture/item/neptunium/NeptuniumHoe.java
@@ -1,73 +1,29 @@
 package com.teammetallurgy.aquaculture.item.neptunium;
 
-import com.mojang.datafixers.util.Pair;
 import com.teammetallurgy.aquaculture.Aquaculture;
 import com.teammetallurgy.aquaculture.init.AquaBlocks;
-import net.minecraft.core.BlockPos;
-import net.minecraft.core.Direction;
-import net.minecraft.sounds.SoundEvents;
-import net.minecraft.sounds.SoundSource;
-import net.minecraft.world.InteractionResult;
-import net.minecraft.world.entity.player.Player;
+import com.teammetallurgy.aquaculture.init.AquaItems;
 import net.minecraft.world.item.HoeItem;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.Tier;
-import net.minecraft.world.item.context.UseOnContext;
-import net.minecraft.world.level.Level;
-import net.minecraft.world.level.block.Blocks;
-import net.minecraft.world.level.block.state.BlockState;
-import net.minecraftforge.event.ForgeEventFactory;
+import net.minecraftforge.common.ToolType;
+import net.minecraftforge.event.world.BlockEvent;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
 
-import javax.annotation.Nonnull;
-import java.util.function.Consumer;
-import java.util.function.Predicate;
-
+@Mod.EventBusSubscriber(modid = Aquaculture.MOD_ID)
 public class NeptuniumHoe extends HoeItem {
 
     public NeptuniumHoe(Tier tier, int damage, float speed) {
         super(tier, damage, speed, new Item.Properties().tab(Aquaculture.GROUP));
     }
 
-    @Override
-    @Nonnull
-    public InteractionResult useOn(UseOnContext useContext) {
-        Level level = useContext.getLevel();
-        BlockPos pos = useContext.getClickedPos();
-        BlockState state = level.getBlockState(pos);
-        int hook = ForgeEventFactory.onHoeUse(useContext);
-        if (hook != 0) {
-            if (hook < 0) {
-                return InteractionResult.FAIL;
-            }
-            if (state == Blocks.FARMLAND.defaultBlockState()) {
-                level.setBlock(pos, AquaBlocks.FARMLAND.defaultBlockState(), 11);
-            }
-            return InteractionResult.SUCCESS;
-        } else {
-            if (useContext.getClickedFace() != Direction.DOWN && level.isEmptyBlock(pos.above())) {
-                Pair<Predicate<UseOnContext>, Consumer<UseOnContext>> pair = TILLABLES.get(level.getBlockState(pos).getBlock());
-                if (pair != null) {
-                    Predicate<UseOnContext> predicate = pair.getFirst();
-                    Consumer<UseOnContext> consumer = pair.getSecond();
-                    if (predicate.test(useContext)) {
-                        Player player = useContext.getPlayer();
-                        level.playSound(player, pos, SoundEvents.HOE_TILL, SoundSource.BLOCKS, 1.0F, 1.0F);
-                        if (!level.isClientSide) {
-                            consumer.accept(useContext);
-                            if (state == Blocks.FARMLAND.defaultBlockState()) {
-                                level.setBlock(pos, AquaBlocks.FARMLAND.defaultBlockState(), 11);
-                            } else {
-                                level.setBlock(pos, state, 11);
-                            }
-                            if (player != null) {
-                                useContext.getItemInHand().hurtAndBreak(1, player, (livingEntity) -> livingEntity.broadcastBreakEvent(useContext.getHand()));
-                            }
-                        }
-                        return InteractionResult.sidedSuccess(level.isClientSide);
-                    }
-                }
-            }
-            return InteractionResult.PASS;
+    @SubscribeEvent
+    public static void blockToolInteract(BlockEvent.BlockToolInteractEvent event) {
+        System.out.println("Interact");
+        if (event.getToolType() == ToolType.HOE && event.getHeldItemStack().getItem() == AquaItems.NEPTUNIUM_HOE) {
+            System.out.println("blockToo");
+            event.setFinalState(AquaBlocks.FARMLAND.defaultBlockState());
         }
     }
 }
